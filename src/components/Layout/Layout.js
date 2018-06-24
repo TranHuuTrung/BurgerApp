1e60b412b76d24dee221bb9a82d57893e55b50c9
TranHuuTrung <tranhuutrung1408@gmail.com>
2018-06-24 10:50:15 +0700

    sideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Layout.css';
 
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <main className={classes.Content}>
-            { props.children }
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-export default layout;
+    closeSideDrawerHandler = () => {
+        this.setState({ showSideDrawer: false});
+    }
+    sideDrawerToggleHandler = () => {
+        this.setState({ showSideDrawer: !this.state.showSideDrawer});
+    }
+    render(){
+        return (
+            <Aux>
+                <Toolbar sideDrawerToggle={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.closeSideDrawerHandler}/>
+                <main className={classes.Content}>
+                    { this.props.children }
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,14 +3,17 @@ import classes from './Toolbar.css';
 
 import Logo from '../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const Toolbar = (props) => (
     <header className={classes.Toolbar} style={{justifyContent: 'space-between', display:'flex'}}>
-        <div>Menu</div>
-        <Logo />
-        <nav>
+        <DrawerToggle clicked={props.sideDrawerToggle}/>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <div className={classes.DesktopOnly}>
             <NavigationItems />
-        </nav>
+        </div>
     </header>
 );
 
